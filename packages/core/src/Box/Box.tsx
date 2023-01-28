106904e18d00872c53b5f05a429e8ed84be5b088
p7gg <69444613+p7gg@users.noreply.github.com>
2023-01-28 13:43:51 -0300

    feat(box): add focus styles

--- packages/core/src/Box/Box.tsx
+++ packages/core/src/Box/Box.tsx
@@ -5,21 +5,33 @@ import { combineProps, combineStyle } from '@solid-primitives/props'
 
 import { clsx } from 'clsx'
 
+import { focusStyles } from '../css'
 import { atomicStyles, AtomicStylesProps } from '../theme'
 import { createPolymorphicComponent } from '../utils'
+import { useWishTheme } from '../WishProvider'
 
-export type BoxProps = AtomicStylesProps
+import type { WishColor } from '../constants'
+
+export interface BoxProps extends AtomicStylesProps {
+  /** Color applied to focus-ring */
+  colorScheme: WishColor
+}
 
 export const Box = createPolymorphicComponent<'div', BoxProps>((_props) => {
+  const theme = useWishTheme()
   const props = combineProps(
     {
       as: 'div',
+      get colorScheme() {
+        return _props.colorScheme ?? theme.primaryColor
+      },
     },
     _props,
   )
-  const [local, atomics, others] = splitProps(
+  const [local, variants, atomics, others] = splitProps(
     props,
     ['as', 'class', 'style'],
+    ['colorScheme'],
     [...atomicStyles.properties.keys()],
   )
   const atoms = createMemo(() => atomicStyles(atomics))
@@ -27,7 +39,7 @@ export const Box = createPolymorphicComponent<'div', BoxProps>((_props) => {
   return (
     <Dynamic
       component={local.as}
-      class={clsx(atoms().className, local.class)}
+      class={clsx(focusStyles(variants), atoms().className, local.class)}
       style={combineStyle(atoms().style, local.style ?? {})}
       {...others}
     />
