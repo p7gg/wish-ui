dd0c2c098530e92b67578faeb96ea4cf13cda7ea
Gabriel Silva <69444613+p7gg@users.noreply.github.com>
2023-02-02 20:05:13 -0300

    v0.1.1 (#20)

---
+++ .changeset/hip-nails-wave.md
@@ -0,0 +1,6 @@
+---
+'@wish-ui/core': patch
+'@wish-ui/vite-plugin': patch
+---
+
+Package fixes
--- packages/core/src/Button/ButtonGroup/ButtonGroup.tsx
+++ packages/core/src/Button/ButtonGroup/ButtonGroup.tsx
@@ -29,33 +29,36 @@ const ButtonGroupContext = createContext<ButtonGroupContextValue>()
 export const useButtonGroup = () => useContext(ButtonGroupContext)
 
 export interface ButtonGroupProps extends BoxProps, Partial<ButtonGroupContextValue> {}
+export interface ButtonGroupCompositions {}
 
-export const ButtonGroup = createComponent<'div', ButtonGroupProps>((_props) => {
-  const props = combineProps(
-    {
-      orientation: 'horizontal',
-      buttonBorderWidth: 1,
-    } as const,
-    _props,
-  )
-  const [local, variants, others] = splitProps(
-    props,
-    ['class'],
-    ['orientation', 'buttonBorderWidth'],
-  )
+export const ButtonGroup = createComponent<'div', ButtonGroupProps, ButtonGroupCompositions>(
+  (_props) => {
+    const props = combineProps(
+      {
+        orientation: 'horizontal',
+        buttonBorderWidth: 1,
+      } as const,
+      _props,
+    )
+    const [local, variants, others] = splitProps(
+      props,
+      ['class'],
+      ['orientation', 'buttonBorderWidth'],
+    )
 
-  const value = {
-    get orientation() {
-      return variants.orientation
-    },
-    get buttonBorderWidth() {
-      return variants.buttonBorderWidth
-    },
-  }
+    const value = {
+      get orientation() {
+        return variants.orientation
+      },
+      get buttonBorderWidth() {
+        return variants.buttonBorderWidth
+      },
+    }
 
-  return (
-    <ButtonGroupContext.Provider value={value}>
-      <Box class={clsx(classes.root[variants.orientation], local.class)} {...others} />
-    </ButtonGroupContext.Provider>
-  )
-})
+    return (
+      <ButtonGroupContext.Provider value={value}>
+        <Box class={clsx(classes.root[variants.orientation], local.class)} {...others} />
+      </ButtonGroupContext.Provider>
+    )
+  },
+)
