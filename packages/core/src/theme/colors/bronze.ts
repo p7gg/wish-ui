517b788a8df3895c9233dc259ceba5185b3ab0b5
Gabriel Silva <69444613+p7gg@users.noreply.github.com>
2023-02-05 23:57:21 -0300

    feat(core): remove radix colors and replace with local refs (#35)
    * feat(core): remove radix colors and replace with local refs
    
    * chore: add changeset

---
+++ packages/core/src/theme/colors/bronze.ts
@@ -0,0 +1,29 @@
+export const bronze = {
+  bronze1: 'hsl(15, 30.0%, 99.1%)',
+  bronze2: 'hsl(17, 63.6%, 97.8%)',
+  bronze3: 'hsl(17, 42.1%, 95.2%)',
+  bronze4: 'hsl(17, 35.2%, 92.1%)',
+  bronze5: 'hsl(17, 31.5%, 88.2%)',
+  bronze6: 'hsl(17, 29.0%, 83.0%)',
+  bronze7: 'hsl(17, 26.9%, 75.6%)',
+  bronze8: 'hsl(17, 25.1%, 66.5%)',
+  bronze9: 'hsl(17, 20.0%, 54.0%)',
+  bronze10: 'hsl(17, 18.1%, 50.1%)',
+  bronze11: 'hsl(15, 20.0%, 43.1%)',
+  bronze12: 'hsl(12, 22.0%, 21.5%)',
+}
+
+export const bronzeDark = {
+  bronze1: 'hsl(17, 10.0%, 8.8%)',
+  bronze2: 'hsl(15, 14.8%, 10.6%)',
+  bronze3: 'hsl(15, 16.3%, 14.3%)',
+  bronze4: 'hsl(16, 17.1%, 17.2%)',
+  bronze5: 'hsl(16, 17.6%, 19.6%)',
+  bronze6: 'hsl(16, 18.1%, 22.9%)',
+  bronze7: 'hsl(17, 18.8%, 28.8%)',
+  bronze8: 'hsl(17, 19.6%, 38.0%)',
+  bronze9: 'hsl(17, 20.0%, 54.0%)',
+  bronze10: 'hsl(18, 24.0%, 59.0%)',
+  bronze11: 'hsl(18, 35.0%, 68.5%)',
+  bronze12: 'hsl(18, 57.0%, 94.1%)',
+}
